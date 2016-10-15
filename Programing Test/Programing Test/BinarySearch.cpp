a950320f22e8fa7e6038f9bf21dc339bb210a3f9
Suri <awumsuri@gmail.com>
2016-10-15 19:29:50 -0400

    New questions

---
+++ Programing Test/Programing Test/BinarySearch.cpp
@@ -0,0 +1,9 @@
+//
+//  BinarySearch.cpp
+//  Programing Test
+//
+//  Created by Suri on 10/14/16.
+//  Copyright © 2016 Suri. All rights reserved.
+//
+
+#include "BinarySearch.hpp"
---
+++ Programing Test/Programing Test/BinarySearch.hpp
@@ -0,0 +1,14 @@
+//
+//  BinarySearch.hpp
+//  Programing Test
+//
+//  Created by Suri on 10/14/16.
+//  Copyright © 2016 Suri. All rights reserved.
+//
+
+#ifndef BinarySearch_hpp
+#define BinarySearch_hpp
+
+#include <stdio.h>
+
+#endif /* BinarySearch_hpp */
